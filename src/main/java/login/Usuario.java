99416b0d709fbdae42325c5e62858c16fa88ae27
Ricardo <ricardomr8@hotmail.com>
2022-09-07 10:50:11 -0300

    Initial commit

---
+++ src/main/java/login/Usuario.java
@@ -0,0 +1,10 @@
+package login;
+
+public abstract class Usuario {
+    private String cpf;
+    private String name;
+    private String senha;
+    private String cidade;
+    private char sexo;
+}
+
